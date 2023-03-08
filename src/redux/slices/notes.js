d64a046e3d161505600a9d672e453b512cf84aaf
vectorg3 <diglytail@gmail.com>
2023-03-08 09:53:48 +0300

    getItem fixed

--- src/redux/slices/notes.js
+++ src/redux/slices/notes.js
@@ -3,10 +3,9 @@ import { createSlice } from '@reduxjs/toolkit';
 const notesSlice = createSlice({
     name: 'notes',
     initialState: {
-        notesList:
-            JSON.parse(localStorage.getItem('notes')).length !== 0
-                ? JSON.parse(localStorage.getItem('notes'))
-                : [{ id: 1123256161558, text: 'bla-bla-bla' }],
+        notesList: JSON.parse(localStorage.getItem('notes'))
+            ? JSON.parse(localStorage.getItem('notes'))
+            : [{ id: 1123256161558, text: 'bla-bla-bla' }],
     },
     reducers: {
         addNote(state, action) {
